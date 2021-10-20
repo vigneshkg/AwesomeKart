10abead8950c213ff0a6b84ada2b2b885c8b21e0
vigneshkg <kgvignesh07@gmail.com>
2021-10-20 22:07:42 +0530

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  userName = new FormControl('');
+  passWord = new FormControl('');
 }
